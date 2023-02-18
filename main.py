3b7517ccdcf0834af706c6b446adc6bd4a14fb03
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-02-19 00:20:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,121 +1,32 @@
-import os
-import requests
-import urllib.error
-import urllib.parse
-
-from pathlib import Path
+from fetch_xkds_image import fetch_xkds_comics
+from send_wall_post_vk import get_wall_upload_server, upload_image,save_wall_photo, send_wall_post
 from environs import Env
 
 
-env = Env()
-env.read_env()
+def main():
+    env = Env()
+    env.read_env()
 
-grup_id = env('group_id')
+    vk_grup_id = env('GROUP_ID')
+    vk_token = env('ACCESS_TOKEN')
 
-def download_img(img_url, images_path):
-    Path('images').mkdir(parents=True, exist_ok=True)
-    response = requests.get(img_url)
-    response.raise_for_status()
+    message = fetch_xkds_comics()
 
-    with open(f'{images_path}', 'wb') as file:
-        file.write(response.content)
+    server_link = get_wall_upload_server(vk_grup_id, vk_token)
 
+    img_link = upload_image(server_link)
 
-def separate_extension(link):
-    url_encoding = urllib.parse.unquote(link)
-    url_parse = urllib.parse.urlsplit(url_encoding)
-    filename = os.path.split(url_parse.path)[1]
-    filename_ext = os.path.splitext(filename)[1]
-    return filename_ext
+    photo_param = img_link['photo']
+    server_param = img_link['server']
+    hash_param = img_link['hash']
 
+    photo_link = save_wall_photo(vk_grup_id, vk_token, photo_param, server_param, hash_param)
 
-def fetch_xkds_comics():
-    num = 1
-    while True:
-        try:
-            url = f'https://xkcd.com/{num}/info.0.json'
+    owner_id = photo_link['response'][0]['owner_id']
+    media_id = photo_link['response'][0]['id']
 
-            if num == 404:
-                num = 405
-                continue
-
-            response = requests.get(url)
-            response.raise_for_status()
-            comic_link = response.json()
-            img_link = comic_link['img']
-            comment_comic = comic_link['alt']
-            filename_ext = separate_extension(img_link)
-            img_path = f'./images/comics_{num}{filename_ext}'
-            # download_img(img_link, img_path)
-            num += 1
-            print(comment_comic)
-
-        except urllib.error.HTTPError:
-            print('картинки закончились')
-            break
-
-def get_server():
-    url = 'https://api.vk.com/method/photos.getWallUploadServer'
-    params = {
-        'group_id': env('group_id'),
-        'access_token': env('access_token'),
-        'v': 5.131
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    server = response.json()['response']['upload_url']
-    return server
-
-server = get_server()
-def get_loading_img():
-    with open('images/comics_1.jpg', 'rb') as file:
-        url = server
-        files = {
-            'photo': file
-        }
-        response = requests.post(url, files=files)
-        response.raise_for_status()
-        return response.json()
-
-loading_img = get_loading_img()
-
-photo_img = loading_img['photo']
-server_img = loading_img['server']
-hash_img = loading_img['hash']
+    send_wall_post(vk_grup_id, vk_token, owner_id, media_id, message)
 
 
-def get_save_wall():
-    url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    params = {
-        'group_id': env('group_id'),
-        'access_token': env('access_token'),
-        'photo': photo_img,
-        'server': server_img,
-        'hash': hash_img,
-        'v': 5.131
-    }
-    response = requests.post(url, params=params)
-    response.raise_for_status()
-    return response.json()
-
-owner_id = get_save_wall()['response'][0]['owner_id']
-media_id = get_save_wall()['response'][0]['id']
-
-def get_wall_post():
-    url = 'https://api.vk.com/method/wall.post'
-    params = {
-        'owner_id': f'-{grup_id}',
-        'access_token': env('access_token'),
-        'from_group': 1,
-        'attachments': f'photo{owner_id}_{media_id}',
-        'message': "Don't we all.",
-        'v': 5.131
-    }
-
-    response = requests.post(url, params=params)
-    response.raise_for_status()
-
-
-get_wall_post()
-
-
+if __name__ == '__main__':
+    main()
