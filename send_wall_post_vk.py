1fb215fb15989f1f7a61adfeeba05f49b7ab8a34
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-02-27 17:58:38 +0300

    Update send_wall_post_vk.py

--- send_wall_post_vk.py
+++ send_wall_post_vk.py
@@ -1,11 +1,10 @@
-import os
 import requests
 
 
-def get_wall_upload_server(vk_grup_id, vk_token):
+def get_wall_upload_server(vk_group_id, vk_token):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     params = {
-        'group_id': vk_grup_id,
+        'group_id': vk_group_id,
         'access_token': vk_token,
         'v': 5.131
     }
@@ -15,26 +14,19 @@ def get_wall_upload_server(vk_grup_id, vk_token):
     return server_link
 
 
-def upload_image(server_link):
-    images = os.walk('images')
-    for img in images:
-        img_name = img[2][0]
-
-        with open(f'images/{img_name}', 'rb') as file:
-            url = server_link
-            files = {
-                'photo': file
-            }
-            response = requests.post(url, files=files)
-            response.raise_for_status()
-            os.remove(f'images/{img_name}')
-            return response.json()
+def upload_image(server_link, img_name):
+    with open(f'images/{img_name}', 'rb') as file:
+        files = {'photo': file}
+        url = server_link
+        response = requests.post(url, files=files)
+    response.raise_for_status()
+    return response.json()
 
 
-def save_wall_photo(vk_grup_id, vk_token, photo_param, server_param, hash_param):
+def save_wall_photo(vk_group_id, vk_token, photo_param, server_param, hash_param):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     params = {
-        'group_id': vk_grup_id,
+        'group_id': vk_group_id,
         'access_token': vk_token,
         'photo': photo_param,
         'server': server_param,
@@ -46,10 +38,10 @@ def save_wall_photo(vk_grup_id, vk_token, photo_param, server_param, hash_param)
     return response.json()
 
 
-def send_wall_post(vk_grup_id, vk_token, owner_id, media_id, message):
+def send_wall_post(vk_group_id, vk_token, owner_id, media_id, message):
     url = 'https://api.vk.com/method/wall.post'
     params = {
-        'owner_id': f'-{vk_grup_id}',
+        'owner_id': f'-{vk_group_id}',
         'access_token': vk_token,
         'from_group': 1,
         'attachments': f'photo{owner_id}_{media_id}',
